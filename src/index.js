b5daeb4a7010865fee538a9073fc8b76260d9d4b
Dmitriy Popko <popkodmitrii@yandex.ru>
2020-05-19 13:55:02 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+
+// d686c76-f171-4f4c-a99e-0bd14c025262
+
+ymaps.ready(init);
+
+function init() {
+    // Создание карты.
+    var myMap = new ymaps.Map("map", {
+        center: [55.76, 37.64],
+        zoom: 7
+    });
+}
